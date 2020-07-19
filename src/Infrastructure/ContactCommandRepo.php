4187922fa3b1ca5d6dce70d1ec68982eafb5ff7a
ntorga <northontorga@gmail.com>
2020-07-19 20:36:57 -0300

    add command repo shell

---
+++ src/Infrastructure/ContactCommandRepo.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure;
+
+use App\Domain\Repository\ContactCommandRepositoryInterface;
+use App\Domain\Entity\Contact;
+use App\Domain\ValueObject\ContactId;
+use App\Domain\ValueObject\Nickname;
+use App\Domain\ValueObject\PersonName;
+use App\Domain\ValueObject\PhoneNumber;
+
+class ContactCommandRepository implements ContactCommandRepositoryInterface
+{
+    public function addContact(
+        PersonName $name,
+        Nickname $nickname,
+        PhoneNumber $phoneNumber
+    ): Contact {
+    }
+    public function updateContact(
+        Contact $updatedContact
+    ): void {
+    }
+    public function removeContact(
+        ContactId $contactId
+    ): void {
+    }
+}
