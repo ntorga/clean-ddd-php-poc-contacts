e0119052fb3ac53cd589a8491ce0715cd6dc518d
ntorga <northontorga@gmail.com>
2020-08-29 16:10:19 -0300

    import dotenv on app

--- src/Presentation/Api/App.php
+++ src/Presentation/Api/App.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 // Instantiate Composer.
 require __DIR__ . '/../../../vendor/autoload.php';
 
+use Dotenv\Dotenv;
 use Slim\Factory\AppFactory;
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../../../config", '.env');
+$dotenv = Dotenv::createImmutable(__DIR__ . "/../../../config", '.env');
 $dotenv->load();
 
 $app = AppFactory::create();
