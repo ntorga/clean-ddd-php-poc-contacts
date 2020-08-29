c82e03dd03ac0c4cf7bf4c6c4cf2eedd4f81603b
ntorga <northontorga@gmail.com>
2020-08-29 20:34:20 -0300

    add controllers to routes

--- src/Presentation/Api/Routes.php
+++ src/Presentation/Api/Routes.php
@@ -4,22 +4,30 @@ declare(strict_types=1);
 
 namespace App\Presentation\Api;
 
-use Slim\App;
-use Psr\Http\Message\ServerRequestInterface as Request;
+use App\Presentation\Api\Controller\AddContactController;
+use App\Presentation\Api\Controller\GetContactController;
+use App\Presentation\Api\Controller\GetContactsController;
+use App\Presentation\Api\Controller\RemoveContactController;
+use App\Presentation\Api\Controller\UpdateContactController;
 use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
 use Slim\Exception\HttpNotFoundException;
 use Slim\Routing\RouteCollectorProxy;
 
-return function (App $app) {
+return static function (App $app) {
     /**
      * Slim4 recommended options for handling CORS.
      * @source http://www.slimframework.com/docs/v4/cookbook/enable-cors.html
      */
-    $app->options('/{routes:.+}', function (Request $request, Response $response) {
+    $app->options('/{routes:.+}', static function (
+        Request $request,
+        Response $response
+    ) {
         return $response;
     });
 
-    $app->add(function ($request, $handler) {
+    $app->add(static function ($request, $handler) {
         $response = $handler->handle($request);
         return $response
             ->withHeader('Access-Control-Allow-Origin', '*')
@@ -27,33 +35,47 @@ return function (App $app) {
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, HEAD, OPTIONS');
     });
 
-    $app->group('/v1/contact', function (RouteCollectorProxy $group) {
-        $group->get('/', function (Request $request, Response $response): Response {
+    $app->group('/v1/contact', static function (RouteCollectorProxy $group) {
+        $group->get('', static function (
+            Request $request,
+            Response $response
+        ): Response {
+            return (new GetContactsController($response))->action();
         });
 
-        $group->post('/', function (Request $request, Response $response): Response {
-            return (new Controller\AddContactController($request, $response))->action();
+        $group->post('', static function (
+            Request $request,
+            Response $response
+        ): Response {
+            return (new AddContactController($request, $response))->action();
         });
 
-        $group->get('/{id:[0-9]+}', function (
+        $group->get('/{id:[0-9]+}', static function (
             Request $request,
             Response $response,
             array $args
         ): Response {
+            return (new GetContactController($response, $args))->action();
         });
 
-        $group->put('/{id:[0-9]+}', function (
+        $group->put('/{id:[0-9]+}', static function (
             Request $request,
             Response $response,
             array $args
         ): Response {
+            return (new UpdateContactController(
+                $request,
+                $response,
+                $args
+            ))->action();
         });
 
-        $group->delete('/{id:[0-9]+}', function (
+        $group->delete('/{id:[0-9]+}', static function (
             Request $request,
             Response $response,
             array $args
         ): Response {
+            return (new RemoveContactController($response, $args))->action();
         });
     });
 
@@ -65,7 +87,7 @@ return function (App $app) {
     $app->map(
         ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
         '/{routes:.+}',
-        function (Request $request): Response {
+        static function (Request $request): Response {
             throw new HttpNotFoundException($request);
         }
     );
