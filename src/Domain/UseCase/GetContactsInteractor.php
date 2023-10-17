ee72fec49a7332e84bc4b45302b6f8a0ebcbb87c
ntorga <northontorga@gmail.com>
2023-10-17 20:16:25 -0300

    refactor: get contacts uc

--- src/Domain/UseCase/GetContactsInteractor.php
+++ src/Domain/UseCase/GetContactsInteractor.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace App\Domain\UseCase;
 
-use App\Domain\Entity\Contact;
 use App\Domain\Repository\ContactQueryRepositoryInterface;
-use App\Domain\ValueObject\ContactId;
-use App\Domain\ValueObject\Nickname;
-use App\Domain\ValueObject\PersonName;
-use App\Domain\ValueObject\PhoneNumber;
+use RuntimeException;
 use Throwable;
 
 class GetContactsInteractor
@@ -18,27 +14,17 @@ class GetContactsInteractor
 
     public function __construct(
         ContactQueryRepositoryInterface $queryRepo
-    )
-    {
+    ) {
         $this->queryRepo = $queryRepo;
     }
 
     public function action(): array
     {
-        $contactsRaw = $this->queryRepo->getContacts();
-        $contacts = [];
-        foreach ($contactsRaw as $contact) {
-            try {
-                $contacts[] = new Contact(
-                    new ContactId((int)$contact["id"]),
-                    new PersonName($contact["name"]),
-                    new Nickname($contact["nickname"]),
-                    new PhoneNumber($contact["phone"])
-                );
-            } catch (Throwable $th) {
-                continue;
-            }
+        try {
+            return $this->queryRepo->get();
+        } catch (Throwable $e) {
+            error_log($e->getMessage());
+            throw new RuntimeException('GetContactsInfraError');
         }
-        return $contacts;
     }
 }
