7de94fde578aecba35cdb2cad199d7d0c4e90045
ntorga <northontorga@gmail.com>
2023-10-17 20:31:14 -0300

    fix: use update contact dto

--- src/Domain/Repository/ContactCommandRepositoryInterface.php
+++ src/Domain/Repository/ContactCommandRepositoryInterface.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 namespace App\Domain\Repository;
 
 use App\Domain\Dto\AddContact;
-use App\Domain\Entity\Contact;
+use App\Domain\Dto\UpdateContact;
 use App\Domain\ValueObject\ContactId;
 
 interface ContactCommandRepositoryInterface
 {
     public function add(AddContact $addContact): void;
     public function remove(ContactId $contactId): void;
-    public function update(Contact $updatedContact): void;
+    public function update(UpdateContact $updatedContact): void;
 }
