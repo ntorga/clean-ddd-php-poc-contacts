34d3df5ba636062c1cef43e3465bed5101ff34aa
ntorga <northontorga@gmail.com>
2023-10-24 21:01:02 -0300

    feat: add check if contact exists before removal

--- src/Domain/UseCase/RemoveContact.php
+++ src/Domain/UseCase/RemoveContact.php
@@ -5,22 +5,34 @@ declare(strict_types=1);
 namespace App\Domain\UseCase;
 
 use App\Domain\Repository\ContactCommandRepositoryInterface;
+use App\Domain\Repository\ContactQueryRepositoryInterface;
 use App\Domain\ValueObject\ContactId;
+use DomainException;
 use RuntimeException;
 use Throwable;
 
 class RemoveContact
 {
+    private ContactQueryRepositoryInterface $queryRepo;
     private ContactCommandRepositoryInterface $commandRepo;
 
     public function __construct(
+        ContactQueryRepositoryInterface $queryRepo,
         ContactCommandRepositoryInterface $commandRepo
     ) {
+        $this->queryRepo = $queryRepo;
         $this->commandRepo = $commandRepo;
     }
 
     public function action(ContactId $contactId): void
     {
+        $getContact = new GetContact($this->queryRepo);
+        try {
+            $getContact->action($contactId);
+        } catch (Throwable $th) {
+            throw new DomainException('ContactNotFound');
+        }
+
         try {
             $this->commandRepo->remove($contactId);
         } catch (Throwable $th) {
