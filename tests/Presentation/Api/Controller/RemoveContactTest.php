0bb409c0439b90669625179a9e8ba493c5bbbfc9
ntorga <northontorga@gmail.com>
2023-12-05 20:47:20 -0300

    refactor: remove contact controller unit test

--- tests/Presentation/Api/Controller/RemoveContactTest.php
+++ tests/Presentation/Api/Controller/RemoveContactTest.php
@@ -4,41 +4,40 @@ declare(strict_types=1);
 
 namespace Tests\Presentation\Api\Controller;
 
-use App\Presentation\Api\Controller\RemoveContactController;
+use App\Presentation\Api\Controller\RemoveContact as RemoveContactController;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Psr7\Factory\ResponseFactory;
 use Tests\HttpTestTrait;
-use Tests\InteractorTrait;
+use Tests\Infrastructure\ContactCommandRepositoryTest;
 use Tests\LoadEnvsTrait;
 
-class RemoveContactControllerTest extends TestCase
+class RemoveContactTest extends TestCase
 {
     use LoadEnvsTrait;
     use HttpTestTrait;
-    use InteractorTrait;
 
     private Response $response;
-    private array $args;
 
     public function setUp(): void
     {
         $this->loadEnvs();
-        $this->args = ["id" => 1];
         $this->response = (new ResponseFactory)->createResponse();
     }
 
     public function testRemoveContact(): void
     {
-        $this->addContact();
+        ContactCommandRepositoryTest::addDummyContact();
+
+        $pathArgs = ["id" => 1];
 
         $removeContactController = new RemoveContactController(
             $this->response,
-            $this->args
+            $pathArgs,
         );
         $result = $removeContactController->action();
         self::assertEquals(200, $result->getStatusCode());
 
-        $this->removeContact();
+        ContactCommandRepositoryTest::removeDummyContact();
     }
 }
