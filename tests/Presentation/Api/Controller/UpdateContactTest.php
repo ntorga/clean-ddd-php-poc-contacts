1ee9113edeb73b793ab5ec18c8acad25802356ae
ntorga <northontorga@gmail.com>
2023-12-05 20:55:04 -0300

    refactor: update contact controller unit test

--- tests/Presentation/Api/Controller/UpdateContactTest.php
+++ tests/Presentation/Api/Controller/UpdateContactTest.php
@@ -4,28 +4,31 @@ declare(strict_types=1);
 
 namespace Tests\Presentation\Api\Controller;
 
-use App\Presentation\Api\Controller\UpdateContactController;
+use App\Presentation\Api\Controller\UpdateContact as UpdateContactController;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface;
 use Slim\Psr7\Factory\ResponseFactory;
 use Tests\HttpTestTrait;
-use Tests\InteractorTrait;
+use Tests\Infrastructure\ContactCommandRepositoryTest;
 use Tests\LoadEnvsTrait;
 
-class UpdateContactControllerTest extends TestCase
+class UpdateContactTest extends TestCase
 {
     use LoadEnvsTrait;
     use HttpTestTrait;
-    use InteractorTrait;
 
-    private ServerRequestInterface $request;
     private Response $response;
 
     public function setUp(): void
     {
         $this->loadEnvs();
+        $this->response = (new ResponseFactory)->createResponse();
+    }
+
+    public function testUpdateContact(): void
+    {
         $executionParams = [
+            "id" => 1,
             "name" => "Raymond Stantz",
             "nickname" => "Raymond",
             "phone" => "555-2368"
@@ -34,23 +37,20 @@ class UpdateContactControllerTest extends TestCase
             'PUT',
             '/v1/contact'
         );
-        $this->request = $this->withJson($psr17ServerRequestFactory, $executionParams);
-        $this->response = (new ResponseFactory)->createResponse();
-    }
+        $request = $this->withJson(
+            $psr17ServerRequestFactory,
+            $executionParams,
+        );
 
-    public function testUpdateContact(): void
-    {
-        $this->addContact();
-        $args = ["id" => 1];
+        ContactCommandRepositoryTest::addDummyContact();
 
         $updateContact = new UpdateContactController(
-            $this->request,
+            $request,
             $this->response,
-            $args
         );
         $result = $updateContact->action();
         self::assertEquals(200, $result->getStatusCode());
 
-        $this->removeContact();
+        ContactCommandRepositoryTest::removeDummyContact();
     }
 }
